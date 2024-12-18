a97f6e871ce0b5d15870276d43130c134a32db0c
backEndFriend51 <rr.stoehr@gmx.de>
2024-12-18 20:16:57 +0100

    csv Update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log(5*2);
+console.log(5*3)
 
 const express = require('express')
 const app = express()
@@ -13,45 +13,3 @@ app.listen(port, () => {
 })
 
 
-const https = require('https');
-
-https.get('https://jsonplaceholder.typicode.com/users', res => {
-  let data = [];
-  const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-  console.log('Status Code:', res.statusCode);
-  console.log('Date in Response header:', headerDate);
-
-  res.on('data', chunk => {
-    data.push(chunk);
-  });
-
-  res.on('end', () => {
-    console.log('Response ended: ');
-    const users = JSON.parse(Buffer.concat(data).toString());
-
-
-    let allUsers = [];
-    let i = 0;
-    for(user of users) {
-
-      allUsers[i] = ' id: '+user.id.toString()+' email: '+user.email+' name: '+user.name+' lng: '+user.address.geo.lng+' lat: '+user.address.geo.lat;
-      console.log(`----------------------------------------------------------------------------------------------------------------------------------`);
-//      mailEnding = Object.entries(user).filter(([key, value]) => key == 'email' && value.includes('biz')).toString();
-//      console.log(mailEnding);
-//      if(mailEnding.includes('biz')) {
-      console.log(`----------------------------------------------------------------------------------------------------------------------------------`);
-      let sub = allUsers[i].substring(allUsers[i].indexOf('@'), allUsers[i].indexOf('@') + 20);
-        console.log(`Porsen: ` + sub);
-        console.log(`-----------------------------------------------------------`); 
-//      }
-      i++;
-    }
-  let emailUsers = allUsers.filter(allUser => allUser.includes('biz'))
-  .map(allUser => allUser.substring(allUser.indexOf(' id: '), allUser.indexOf(' lat: ')))
-  .reduce(allUser => allUser.substring(allUser.indexOf(' lng: ') ,allUser.indexOf(' lng: ')+14) +
-                     allUser.substring(allUser.indexOf(' lng: ') ,allUser.indexOf(' lng: ')+14));
-  console.log('Ending '+emailUsers);
-  });
-}).on('error', err => {
-  console.log('Error: ', err.message);
-});
---
+++ script.js
@@ -0,0 +1,50 @@
+const https = require('https');
+
+
+https.get('https://jsonplaceholder.typicode.com/users', res => {
+    let data = [];
+    const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+    console.log('Status Code:', res.statusCode);
+    console.log('Date in Response header:', headerDate);
+
+    res.on('data', chunk => {
+        data.push(chunk);
+    });
+
+    res.on('end', () => {
+        console.log('Response ended: ');
+        const users = JSON.parse(Buffer.concat(data).toString());
+        
+        const filteredAndMapUser = users.filter(user => user.email.includes("biz")).map(u => u.id + " / " + u.email + " / " + u.name + " / " 
+            + u.address.geo.lat + " / " + u.address.geo.lng).reduce(sum);
+        console.log(filteredAndMapUser);
+        function sum(acc, item) {
+            return acc + item;
+        }
+
+        console.log("--------------------------------------------------------------------------------------------------------------------------");
+        const reduseUser = "filteredAndMapUser";
+        console.log(reduseUser);
+
+ /*       let allUsers = [];
+        let i = 0;
+        for (user of users) {
+
+            allUsers[i] = ' id: ' + user.id.toString() + ' email: ' + user.email + ' name: ' + user.name + ' lng: ' + user.address.geo.lng + ' lat: ' + user.address.geo.lat;
+            console.log(`----------------------------------------------------------------------------------------------------------------------------------`);
+            //      mailEnding = Object.entries(user).filter(([key, value]) => key == 'email' && value.includes('biz')).toString();
+            //      console.log(mailEnding);
+            //      if(mailEnding.includes('biz')) {
+            console.log(`----------------------------------------------------------------------------------------------------------------------------------`);
+            let sub = allUsers[i].substring(allUsers[i].indexOf('@'), allUsers[i].indexOf('@') + 20);
+            console.log(`Porsen: ` + sub);
+            console.log(`-----------------------------------------------------------`);
+            //      }
+            i++;
+        }
+        let emailUsers = allUsers.filter(allUser => allUser.includes('biz')).map(allUser => allUser.substring(allUser.indexOf(' id: '), allUser.indexOf(' lat: ')));
+        console.log('Ending ' + emailUsers);*/
+    });
+}).on('error', err => {
+    console.log('Error: ', err.message);
+});
